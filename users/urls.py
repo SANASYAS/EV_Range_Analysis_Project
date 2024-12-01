acabc35d4ebe146147585067196d64b3264d84b3
sanasya <sana@gmail.com>
2024-12-01 14:09:41 +0530

    first commit

---
+++ users/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from .views import register,success_view,login_view,fleet_manager_home_view,driver_home_view,introduction_to_ev,dataset,distribution,relationship,vehicle_status,redirect_to_distribution,prediction_view,relationship_view,home_view,predict_range,predict_electric_range,relationship_analysis
+
+urlpatterns = [
+    path("", home_view, name="home"),
+    path("register/", register, name='register'),
+    path('success/', success_view, name='success_url'),  # Success URL
+    path('login/', login_view, name='login'),
+    # Define other URLs for fleet manager and driver pages
+    path('fleet_manager_home/', fleet_manager_home_view, name='fleet_manager_home'),
+    path('driver_home/', driver_home_view, name='driver_home'),
+    #Fleet Manager Dashboard
+    path('introduction_to_ev/', introduction_to_ev, name='introduction_to_ev'),
+    path('dataset/', dataset, name='dataset'),
+    path('distribution/', distribution, name='distribution'),
+    path('relationship/', relationship, name='relationship'),
+    path('vehicle_status/', vehicle_status, name='vehicle_status'),
+    # path('distribution/', distribution_view, name='distribution'),
+    path('distribution/', redirect_to_distribution, name='distribution'),
+    path('prediction/', prediction_view, name='prediction'),
+    path('relationship/', relationship_view, name='relationship'),
+    path('predict/', predict_electric_range, name='predict_electric_range'),
+    path('predict_range/',predict_range, name='predict_range'),
+    path('relationship_analysis/', relationship_analysis, name='relationship_analysis'),
+]
+
